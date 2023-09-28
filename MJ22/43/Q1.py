ad9d9b8b65c8fd8a493a6601216ca6ab98a7cf39
Fasih Hasan <47947561+fasihh@users.noreply.github.com>
2023-09-28 20:16:00 +0500

    Update Q1.py

--- MJ22/43/Q1.py
+++ MJ22/43/Q1.py
@@ -1,68 +1,54 @@
-# DEC playerScore[1:11] : INTEGER
-# DEC playerName[1:11] : STRING
-playerScore = ["" for a in range(11)]
-playerName = [0 for b in range(11)]
+players = [["", 0] for i in range(11)]
 
 
-def ReadHighScore():
-    global playerScore, playerName
-
-    playerFile = open("HighScore.txt", "rt")
-
-    for i in range(10):
-        name = playerFile.readline()
-        score = playerFile.readline()
-
-        playerName[i] = name.strip()
-        playerScore[i] = int(score.strip())
-
-
+def ReadHighScores():
+    with open("Highscore.txt") as f:
+        i = 0
+        for l in f:
+            players[i] = [l.strip(), int(next(f).strip())]
+            i += 1
+            
 def OutputHighScores():
     for i in range(10):
-        print(playerName[i], playerScore[i])
-
-
-def main():
-    global playerScore, playerName
-
-    def WriteTopTen():
-        try:
-            newScoreFile = open("NewHighScore.txt", "x")
-        except FileExistsError:
-            newScoreFile = open("NewHighScore.txt", "wt")
-
-        for i in range(10):
-            name = playerName[i] + "\n"
-            score = str(playerScore[i]) + "\n"
-            newScoreFile.write(name)
-            newScoreFile.write(score)
-
-    def adjustScores(name, score):
-        for i in range(10):
-            if score > playerScore[i]:
-                tempScore = playerScore[i]
-                tempName = playerName[i]
-                playerScore[i] = score
-                playerName[i] = name
-                score = tempScore
-                name = tempName
-
-    ReadHighScore()
+        print(players[i][0], players[i][1])
+        
+def Procedure(name, score):
+    players[10] = [name, score]
+    
+    for i in range(len(players)):
+        j = i
+        while players[j - 1][1] < players[j][1] and j > 0:
+             players[j - 1], players[j] = players[j], players[j - 1]
+             j -= 1
+             
+def WriteTopTen():
+    with open("NewHighScore.txt", "w") as f:
+        for i in range(9):
+            f.write(f"{players[i][0]}\n{players[i][1]}\n")
+        f.write(f"{players[i][0]}\n{players[i][1]}")
+         
+def main():  
+    ReadHighScores()
     OutputHighScores()
-
-    userName = input("Input 3 character player name: ")
-    while len(userName) != 3:
-        userName = input("Input 3 character player name: ")
-
-    userScore = int(input("input player score: "))
-    while 1 > userScore < 100000:
-        userScore = int(input("input player score: "))
-
-    adjustScores(userName, userScore)
-
+    print()
+    
+    while True:
+        name = input("Enter player name: ")
+        
+        if len(name) == 3:
+            break
+        
+    while True: 
+        score = int(input("Enter player score: "))
+        
+        if score <= 100000 or score > 0:
+            break
+        
+    Procedure(name, score)
+    
+    print()
     OutputHighScores()
-
     WriteTopTen()
-
-
+        
+        
 main()
